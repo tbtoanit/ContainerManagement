8c115a34f52d6f2d490d2828ca075c13c21fe2cd
Trần Bảo Toàn <49049899+tbtoanit@users.noreply.github.com>
2019-10-13 18:19:04 +0700

    The first version for calculation

---
+++ src/main/java/myPackage/Calculation.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class Calculation {
+    //The first version for calculation
+    public static void main(String[] args) {
+        System.out.println(additional(1, 2));
+    }
+
+    public static int additional(int a, int b){
+        return  a + b;
+    }
+}
